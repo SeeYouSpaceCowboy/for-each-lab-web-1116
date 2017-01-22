4fce1d483b4a77044c26b146b6546f485c008675
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-01-22 10:07:40 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  array.forEach((element, index, array) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var colors = ["Red", "Blue", "Green", "Yellow"]
+  colors.forEach(callback)
+  return colors
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
